7a32225fdfa7713d1e7b129af0099466efc933c4
neagewatch <memk.mame@gmail.com>
2026-08-22 07:43:54 +0900

    Add nationwide travel TV region channels

---
+++ kyoto-tv/national.js
@@ -0,0 +1,18 @@
+window.NATIONAL_CHANNELS=[
+{num:101,name:'渋谷 夕方ウォーク',id:'l_IELqhmNIg',title:'Tokyo Shibuya evening walk 4K HDR',meta:'渋谷の夕方',cat:'街歩き',area:'渋谷',region:'東京',lat:35.6595,lng:139.7005,season:'通年'},
+{num:102,name:'渋谷 10分セレクト',id:'l_IELqhmNIg',title:'渋谷ウォーク・10分地点から',meta:'短時間で東京の雰囲気',cat:'スポット直行',area:'渋谷',region:'東京',lat:35.6595,lng:139.7005,start:600,season:'通年'},
+{num:103,name:'渋谷 夜セレクト',id:'l_IELqhmNIg',title:'渋谷ウォーク・20分地点から',meta:'東京の街歩き',cat:'夜の街',area:'渋谷',region:'東京',lat:35.6595,lng:139.7005,start:1200,season:'通年'},
+{num:201,name:'大阪 道頓堀・アメ村',id:'axewALA2s_E',title:'Osaka Dotonbori & Amerikamura 4K HDR',meta:'大阪ミナミ',cat:'街歩き',area:'ミナミ',region:'大阪',lat:34.6687,lng:135.5013,season:'通年'},
+{num:202,name:'アメリカ村 直行',id:'axewALA2s_E',title:'アメリカ村 Fashion Street',meta:'5分地点から',cat:'スポット直行',area:'ミナミ',region:'大阪',lat:34.6722,lng:135.4984,start:300,season:'通年'},
+{num:203,name:'道頓堀 直行',id:'axewALA2s_E',title:'道頓堀 Cosplay Scene',meta:'約66分地点から',cat:'スポット直行',area:'ミナミ',region:'大阪',lat:34.6687,lng:135.5013,start:3960,season:'通年'},
+{num:204,name:'道頓堀 週末夜',id:'05NS69BkFP4',title:'Weekend Night Walk in Dotonbori 4K HDR',meta:'夜の大阪',cat:'夜の街',area:'ミナミ',region:'大阪',lat:34.6687,lng:135.5013,season:'冬'},
+{num:301,name:'奈良公園',id:'0cSugd-4S4k',title:'Nara Park Walking Tour 4K HDR',meta:'鹿と古都を歩く',cat:'王道名所',area:'奈良公園',region:'奈良',lat:34.6850,lng:135.8430,season:'冬'},
+{num:302,name:'奈良公園 10分セレクト',id:'0cSugd-4S4k',title:'奈良公園・10分地点から',meta:'鹿のいる公園散歩',cat:'スポット直行',area:'奈良公園',region:'奈良',lat:34.6850,lng:135.8430,start:600,season:'冬'},
+{num:401,name:'札幌駅→大通公園',id:'KbCrl_i0L6I',title:'Sapporo Station to Odori Park 4K HDR',meta:'札幌中心部を散歩',cat:'街歩き',area:'札幌',region:'北海道',lat:43.0642,lng:141.3469,filmed:'2024-05-18',season:'春'},
+{num:402,name:'札幌時計台 直行',id:'KbCrl_i0L6I',title:'札幌市時計台へ直行',meta:'10:18地点から',cat:'スポット直行',area:'札幌',region:'北海道',lat:43.0626,lng:141.3537,start:618,season:'春'},
+{num:403,name:'大通公園 直行',id:'KbCrl_i0L6I',title:'大通公園へ直行',meta:'14:20地点から',cat:'スポット直行',area:'札幌',region:'北海道',lat:43.0600,lng:141.3544,start:860,season:'春'},
+{num:404,name:'さっぽろテレビ塔 直行',id:'KbCrl_i0L6I',title:'さっぽろテレビ塔へ直行',meta:'16:17地点から',cat:'スポット直行',area:'札幌',region:'北海道',lat:43.0611,lng:141.3564,start:977,season:'春'},
+{num:501,name:'那覇 4Kウォーク',id:'JBiFQcPJZkM',title:'Okinawa Naha 4K Walking Tour',meta:'南国の那覇を歩く',cat:'街歩き',area:'那覇',region:'沖縄',lat:26.2124,lng:127.6809,season:'夏'},
+{num:502,name:'那覇中心部 直行',id:'JBiFQcPJZkM',title:'Naha Central Tourist District',meta:'2:37地点から',cat:'スポット直行',area:'那覇',region:'沖縄',lat:26.2144,lng:127.6792,start:157,season:'夏'},
+{num:503,name:'那覇 夜ウォーク',id:'JBiFQcPJZkM',title:'Okinawa Nightwalk',meta:'51:37地点から',cat:'夜の街',area:'那覇',region:'沖縄',lat:26.2144,lng:127.6792,start:3097,season:'夏'}
+];
